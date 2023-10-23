5751549953ff2179cc3cbc3d62ce0b1841a2cfe4
Ask Sødal <asksodal@gmail.com>
2023-10-23 13:31:15 +0200

    now movement is good

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use bevy_svg::prelude::*;
 
+const PI: f32 = std::f32::consts::PI;
+
 fn main() {
     App::new()
         .insert_resource(Msaa::Sample4)
@@ -13,18 +15,24 @@ fn main() {
         }))
         .add_plugins(bevy_svg::prelude::SvgPlugin)
         .add_systems(Startup, setup)
-        .add_systems(
-            Update,
-            (
-                keyboard_input,
-                // apply_velocity
-            ),
-        )
+        .add_systems(Update, (keyboard_input, apply_velocity))
         .run();
 }
 
 #[derive(Component)]
-struct Player;
+struct Player {
+    dash_cooldown: Timer,
+    dash_duration: Timer,
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        Self {
+            dash_cooldown: Timer::from_seconds(0.8, TimerMode::Once),
+            dash_duration: Timer::from_seconds(0.1, TimerMode::Once),
+        }
+    }
+}
 
 #[derive(Component)]
 struct Enemy;
@@ -79,9 +87,9 @@ fn setup(
         ..default()
     });
     cmd.spawn((
-        Player,
+        Player::default(),
         TransformBundle::default(),
-        // Velocity::default(),
+        Velocity::default(),
         ComputedVisibility::default(),
         Visibility::Visible,
     ))
@@ -119,32 +127,55 @@ fn setup(
 //     for mut trans in query.iter_mut() {}
 // }
 
-// fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>) {
-//     for (mut trans, vel) in query.iter_mut() {
-//         trans.translation.x += vel.0.x;
-//         trans.translation.y += vel.0.y;
-//     }
-// }
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>) {
+    for (mut trans, vel) in query.iter_mut() {
+        trans.translation.x += vel.0.x;
+        trans.translation.y += vel.0.y;
+    }
+}
 
 fn keyboard_input(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Transform, With<Player>>,
+    mut query: Query<(&mut Velocity, &mut Player)>,
+    time: Res<Time>,
 ) {
     const SPEED: f32 = 5.;
-    const DASH_C: f32 = 3.;
+    const DASH_C: f32 = 4.;
 
-    let mut trans = query.single_mut();
+    let (mut vel, mut player) = query.single_mut();
 
-    if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-        trans.translation.x -= SPEED;
+    player.dash_cooldown.tick(time.delta());
+    player.dash_duration.tick(time.delta());
+
+    if keyboard_input.pressed(KeyCode::Space) {
+        if player.dash_cooldown.finished() {
+            player.dash_duration.reset();
+        }
+        player.dash_cooldown.reset();
     }
-    if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-        trans.translation.x += SPEED;
-    }
-    if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
-        trans.translation.y += SPEED;
-    }
-    if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
-        trans.translation.y -= SPEED;
-    }
+
+    let speed = if player.dash_duration.finished() {
+        SPEED
+    } else {
+        SPEED * DASH_C
+    };
+
+    let (up, down, left, right) = (
+        keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S),
+        keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W),
+        keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A),
+        keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D),
+    );
+
+    vel.0 = match (up, down, left, right) {
+        (true, false, false, false) => Vec2::from_angle(3. * PI / 2.),
+        (false, true, false, false) => Vec2::from_angle(PI / 2.),
+        (false, false, true, false) => Vec2::from_angle(PI),
+        (false, false, false, true) => Vec2::from_angle(0.),
+        (false, true, true, false) => Vec2::from_angle(3. * PI / 4.),
+        (false, true, false, true) => Vec2::from_angle(PI / 4.),
+        (true, false, true, false) => Vec2::from_angle(5. * PI / 4.),
+        (true, false, false, true) => Vec2::from_angle(7. * PI / 4.),
+        _ => Vec2::ZERO,
+    } * speed;
 }
